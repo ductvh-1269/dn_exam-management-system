b99e2f6f1a6f4257a48c5e10487c7a8922da6ec3
ductr <tran.van.hoai.duc@sun-asterisk.com>
2022-07-06 10:29:56 +0700

    see own result detail

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -5,10 +5,27 @@ module ExamsHelper
 
   def load_exam_path user
     return :admin_exams if user&.admin?
+
     :exams
   end
 
   def load_user_name exam
     "#{exam.user.first_name} #{exam.user.last_name}"
+  end
+
+  def generate_string first, last
+    "#{first}: #{last}"
+  end
+
+  def choose_answer answer_id, exam_details
+    exam_details.selected_answer_id == answer_id
+  end
+
+  def content_for_span exam_details
+    if Answer.find_by(id: exam_details.selected_answer_id)&.true_answer?
+      {value: t(".correct"), class: "badge badge-pill badge-success"}
+    else
+      {value: t(".incorrect"), class: "badge badge-pill badge-danger"}
+    end
   end
 end
